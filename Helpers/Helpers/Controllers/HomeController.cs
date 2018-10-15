5a8b148036f8034aac174c337fb58a0a9b62bf6b
AlexSmyk1992 <43552130+AlexSmyk1992@users.noreply.github.com>
2018-10-15 15:04:55 +0300

    for 22 module

---
+++ Helpers/Helpers/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using Helpers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Helpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            var books = db.Books;
+            SelectList authors = new SelectList(db.Books, "Author", "Name");
+            ViewBag.Authors = authors;
+            return View();
+        }
+
+        [HttpPost]
+        public string GetForm(string author)
+        {
+            return author;
+        }
+        
+        //public string GetForm(string[] countries)
+        //{
+        //    string result = "";
+        //    foreach (string c in countries)
+        //    {
+        //        result += c;
+        //        result += ";";
+        //    }
+        //    return "Вы выбрали: " + result;
+        //}
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        //[HttpPost]
+        //public string GetForm(string text)
+        //{
+        //    return text;
+        //}
+
+        //[HttpPost]
+        //public string GetForm(string color)
+        //{
+        //    return color;
+        //}
+
+        //[HttpPost]
+        //public string GetForm(bool set)
+        //{
+        //    return set.ToString();
+        //}
+    }
+}
